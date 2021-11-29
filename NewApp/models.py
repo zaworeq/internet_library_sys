7da8ce8d57a234ade5176b2cc7ffcdf65fc5e2e3
zaworeq <zaworskiwiktor570@gmail.com>
2021-11-29 08:29:36 +0100

    First commit

---
+++ NewApp/models.py
@@ -0,0 +1,16 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_date = models.DateField('publication date')
+    isbn_number = models.IntegerField(default=0)
+    page_number = models.IntegerField(default=0)
+    front_page_url = models.URLField(default='http://www.google.com')
+    publication_language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
