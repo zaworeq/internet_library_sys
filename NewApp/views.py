47136600aeeb639a970f2ec900daa833270f00c8
zaworeq <zaworskiwiktor570@gmail.com>
2021-11-29 10:38:28 +0100

    Added listview and created table from db

--- NewApp/views.py
+++ NewApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Book
+
+
+class BookList(ListView):
+    model = Book
+    template_name = "book_list.html"
+
