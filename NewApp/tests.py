a8285ef2c4e014217fc724d992d230f30652a79e
zaworeq <zaworskiwiktor570@gmail.com>
2021-12-01 16:50:35 +0100

    Basic tests

--- NewApp/tests.py
+++ NewApp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Book
+
+
+class BasicTest(TestCase):
+    def setUp(self):
+        self.book = Book()
+        self.book.title = 'Test'
+        self.book.author = 'TestAuthor'
+        self.book.publication_date = '2000-09-24 01:20'
+        self.book.publication_language = 'pl'
+        self.book.save()
+
+    def test_fields(self):
+        book = Book()
+        book.title = 'Test'
+        book.author = 'TestAuthor'
+        book.publication_date = '2000-09-24 01:20'
+        book.publication_language = 'pl'
+        book.save()
+
+        record = Book.objects.get(pk=book.pk)
+        self.assertEqual(record, book)
+
+
+
+
